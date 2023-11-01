7356ac4476c5182f03c487d5e4e830db7da89852
Jakub Neumann <jn@yavi.eu>
2023-11-01 18:42:33 +0100

    Further arithmetics
    Signed-off-by: Jakub Neumann <jn@yavi.eu>

--- src/save_layout.py
+++ src/save_layout.py
@@ -1,4 +1,5 @@
 from functools import cached_property, reduce
+import itertools
 import math
 import time
 
@@ -173,8 +174,9 @@ class ShopBuys:
         wow_pass_idx: int = (sd["WoW Passes"][sd["WoW Passes", "Cost"] - quarks < 0]).idxmax()["Cost"]  # type: ignore[index, assignment]
         accel_idx: int = (sd["Acceleration"][sd["Acceleration", "Cost"] - quarks < 0]).idxmax()["Cost"]  # type: ignore[index, assignment]
 
-        self.wow3, self.wowY, self.wowCost = sd["WoW Passes"].iloc[wow_pass_idx].items()
-        self.accel1, self.accel2, self.accelCost = sd["Acceleration"].iloc[accel_idx].items()
+        self.wow3, self.wowY, self.wowCost = [int(x) for x in sd["WoW Passes"].iloc[wow_pass_idx].tolist()]
+        self.accel1, self.accel2, self.accelCost = [int(x) for x in sd["Acceleration"].iloc[accel_idx].tolist()]
+
 
 class SynergismConfig(BaseModel):
     targetGainPercent: int
@@ -207,8 +209,18 @@ class SynergismGame(BaseModel):
 
     config: Optional[SynergismConfig] = Field(skip=True, required=False, default=None)
 
+    shop_benefit_hept: float = Field(skip=True, required=False, default=0)
+    shop_benefit_accel: float = Field(skip=True, required=False, default=0)
+
     def set_config(self, config: SynergismConfig) -> None:
         self.config = config
+
+    def set_shop_benefits(self, buys: ShopBuys) -> None:
+        if self.total_quarks > 2000:
+            self.shop_benefit_accel = round(1 + 0.01 * buys.accel1 + buys.accel2 * 0.005 + 0.01 * buys.accel1 * buys.accel2 * 0.005, 3)
+        
+        if self.total_quarks > 5000:
+            self.shop_benefit_hept = round(1 +  0.01 * buys.wow3 + buys.wowY * 0.005 + 0.01 * buys.wow3 * buys.wowY * 0.005, 3)
 
     def get_plat_upgrade(self, row: int, column: int) -> int:
         try:
@@ -216,7 +228,7 @@ class SynergismGame(BaseModel):
         except IndexError:
             return 0
 
-    def get_achievment(self, id: int) -> int:
+    def get_achievement(self, id: int) -> int:
         try:
             return self.achievements[id]
         except IndexError:
@@ -252,8 +264,8 @@ class SynergismGame(BaseModel):
         )
         omegaBoost = 1 + 0.002 * sum(self.usedCorruptions) * self.get_plat_upgrade(3, 5)
         chronosBoost = 1 + 0.6 / 1000 * self.hepts.chronos.effective_boost(1000, 1 / 6, 1 / 750 * self.u44)
-        achievementBoost = (1 + min(0.1, 1 / 100 * math.log10(self.ascensionCount + 1)) * self.get_achievment(262)) * (
-            1 + min(0.10, 1 / 100 * math.log10(self.ascensionCount + 1)) * self.get_achievment(263)
+        achievementBoost = (1 + min(0.1, 1 / 100 * math.log10(self.ascensionCount + 1)) * self.get_achievement(262)) * (
+            1 + min(0.10, 1 / 100 * math.log10(self.ascensionCount + 1)) * self.get_achievement(263)
         )
 
         return c15boost * shopUpgrades * omegaBoost * chronosBoost * achievementBoost
@@ -277,5 +289,72 @@ class SynergismGame(BaseModel):
 
         return math.floor(self.quarksLeft) + used_sum - 15
 
+    @cached_property
+    def hept_per_day(self) -> float:
+        if not self.config:
+            raise ValueError("Config not set")
+
+        return self.shop_benefit_hept * self.config.hps * self.multiplier * (86400 + self.config.addUsesPerDay * 60 * self.shop.calculator3)
+
     def buyable_hept(self, hept: Hepteract) -> int:
         return math.floor(self.wowAbyssals)
+
+    @cached_property
+    def orb_to_powder(self) -> float:
+        denominator = math.prod(
+            [
+                1 / 100,
+                1 + 1 / 50 * math.log2(self.challenge15Exponent / (7000000000000000 / 32)),
+                1 + self.shop.powderEX / 50,
+                1 + self.get_achievement(256) / 20,
+                1 + self.get_achievement(257) / 20,
+                1 + 0.01 * self.get_plat_upgrade(4, 1),
+            ]
+        )
+        return 1/denominator
+
+    @property
+    def hepts_small_inc(self) -> float:
+        return self.hepts.chronos.to_level * 0.0000001 / self.orb_to_powder / 250000
+    
+    @property
+    def p44_noname(self) -> float:
+        """ Dimnishing returns reduction value """
+        return (1+0.0000001/2) ** ((1/6)+((1/750)*self.get_plat_upgrade(4, 4)))
+
+    @property
+    def powder_tomorrow(self) -> float:
+        return self.overfluxPowder + self.overfluxOrbs / self.orb_to_powder
+
+    @property
+    def cube_from_powder(self) -> float:
+        if self.powder_tomorrow > 10000:
+            return 1+1/16* (math.log10(self.powder_tomorrow) ** 2) - 1 
+        else:
+            return 1+1/10000*self.powder_tomorrow - 1
+
+    @property
+    def powder_goal(self) -> float:
+        """
+        F5 = small incr in hept
+        G7 = overflux powder
+        E5 = p44_noname
+        C3 = chronos increase
+        E3 = hepts to level
+        G5 = orb_to_powder
+
+        =IF(Formulas!$F$5/((Formulas!G7)^(Formulas!E5-1)-1)<10000,
+            IF(((Formulas!C3+1)^(1/Formulas!E3)-1)*10000*250000*Formulas!G5>1,
+                Formulas!G7+ROUNDDOWN(Formulas!G3/24/250000)/Formulas!G5,10000),
+            (Formulas!$F$5/((Formulas!$F$5/((Formulas!$F$5/((Formulas!$F$5/((Formulas!G7)^(Formulas!E5-1)-1))^(Formulas!E5-1)-1))^(Formulas!E5-1)-1))^(Formulas!E5-1)-1))/(B10+1)*B10)
+        """
+        if self.hepts_small_inc / ((self.overfluxPowder) ** (self.p44_noname - 1) - 1) < 10000:
+            if self.chronos_increase + 1 ** (1 / self.hepts.chronos.to_level) - 1 * 1000 * 250000 * self.orb_to_powder > 1:
+                return self.overfluxPowder + math.floor(self.hept_per_day / 24 / 250000) / self.orb_to_powder
+            else:
+                return 10000
+        return (self.hepts_small_inc / ((self.hepts_small_inc / ((self.hepts_small_inc / ((self.hepts_small_inc / ((self.overfluxPowder) ** (self.p44_noname -1) -1)) ** (self.p44_noname -1) -1)) ** (self.p44_noname -1) -1)) ** (self.p44_noname -1) -1)) / (self.cube_from_powder + 1) * self.cube_from_powder
+
+    @property
+    def orbs_to_powder_goal(self) -> float:
+        return (self.powder_goal - self.overfluxPowder) * self.orb_to_powder - self.overfluxOrbs
--- src/synergisa.py
+++ src/synergisa.py
@@ -2,13 +2,16 @@ import argparse
 import base64
 import json
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 from functools import cached_property
 import pandas
 
 
 from rich.console import Console
 from rich.table import Table
+from rich.columns import Columns
+from rich.panel import Panel
+from rich.text import Text
 from rich import box
 from loguru import logger
 
@@ -26,58 +29,87 @@ def load_base64_file(file_path):
         return decoded_data
 
 
-def print_stats(game: SynergismGame) -> None:
-    print(f"C15: {game.challenge15Exponent:.2e}")
-    print("Cubes    >> ", end="")
-    print(
-        f"WoW {game.wowCubes:<16.2e} Tess {game.wowTesseracts:<16.2e} Hyper {game.wowHypercubes:<16.2e} Plat {game.wowHypercubes:.2e}"
-    )
+def print_balances(game: SynergismGame) -> List[Table]:
+    misc_stats = Table(box=box.MINIMAL_DOUBLE_HEAD, title="Misc Items", title_style="bold")
+    misc_stats.add_column("Stat")
+    misc_stats.add_column("Value")
 
-    print("Hept     >> ", end="")
-    print(f"Chronos {game.hepts.chronos.balance:<12.2e} Abyss {game.hepts.abyss.balance:<16.2e}")
+    misc_stats.add_row("C15 Exponent", f"{game.challenge15Exponent:.2e}")
+    misc_stats.add_row("Overflux Orbs", f"{game.overfluxOrbs:.2e}")
+    misc_stats.add_row("Overflux Powder", f"{game.overfluxPowder:.2e}")
+    misc_stats.add_row("Singularity Count", f"{game.singularityCount:.0f}")
 
-    print("Hept Tier>> ", end="")
-    print(
-        f"Chronos {game.hepts.chronos.tier:<12d} Abyss {game.hepts.abyss.tier:<16d} Accel {game.hepts.accelerator.tier:<16d}"
-    )
+    cubes = Table(box=box.MINIMAL_DOUBLE_HEAD, title="Cubes", title_style="bold")
 
-    print("Overflux >> ", end="")
-    print(f"Orbs {game.overfluxOrbs:.2e}\t Powder {game.overfluxPowder:.2e}")
+    cubes.add_column("Type")
+    cubes.add_column("Amount")
+
+    cubes.add_row("WoW Cubes", f"{game.wowCubes:.2e}")
+    cubes.add_row("Tesseracts", f"{game.wowTesseracts:.2e}")
+    cubes.add_row("Hypercubes", f"{game.wowHypercubes:.2e}")
+    cubes.add_row("Platonic", f"{game.wowPlatonicCubes:.2e}")
+    cubes.add_row("Hepteracts", f"{game.wowAbyssals:.2e}")
 
 
-def check_ratios(game: SynergismGame):
-    print()
-    ratios = Table(title="Ratios", box=box.MINIMAL_DOUBLE_HEAD)
+    hepts_table = Table(box=box.MINIMAL_DOUBLE_HEAD, title="Hepteracts", title_style="bold")
+
+    hepts_table.add_column("Type")
+    hepts_table.add_column("Tier")
+    hepts_table.add_column("Balance")
+
+    for hept_type in game.hepts.model_fields.keys():
+        the_hept = getattr(game.hepts, hept_type)
+        hepts_table.add_row(hept_type.title(), f"{the_hept.tier}", f"{the_hept.balance:.2e}")
+
+    return [hepts_table, cubes, misc_stats]
+
+
+def check_ratios(game: SynergismGame) -> Tuple[Table, List[str]]:
+    ratios = Table(box=box.MINIMAL_DOUBLE_HEAD)
     ratios.add_column("Ratio")
     ratios.add_column("Value")
 
     chronos_accel_ratio = int(game.hepts.chronos.cap / game.hepts.accelerator.cap)
-    if chronos_accel_ratio != 32:
-        print(f"Chronos to Accelerator ratio is {chronos_accel_ratio:d} instead of 32")
-    ratios.add_row("Chronos/Accel:", f"{int(chronos_accel_ratio)}/1")
-
     chronos_challenge_ratio = int(game.hepts.chronos.cap / game.hepts.challenge.cap)
     hyper_challenge_ratio = int(game.hepts.hyperrealism.cap / game.hepts.challenge.cap)
-
-    ratios.add_row("Chronos/Hyper/Challenge:", f"{chronos_challenge_ratio:d}/{hyper_challenge_ratio:d}/1")
-    if chronos_challenge_ratio != 4:
-        print(f"Chronos to Challenge ratio is {chronos_challenge_ratio:d} instead of 4")
-    if hyper_challenge_ratio != 2:
-        print(f"Hyper to Challenge ratio is {hyper_challenge_ratio:d} instead of 2")
-
     accel_mult_ratio = int(game.hepts.accelerator.cap / game.hepts.multiplier.cap)
     boost_mult_ratio = int(game.hepts.acceleratorBoost.cap / game.hepts.multiplier.cap)
 
-    ratios.add_row("Accel/Boost/Mult:", f"{accel_mult_ratio:d}/{boost_mult_ratio:d}/1")
+    messages: List[str] = []
+
+    e_chronos_accel = False
+    e_chronos_challenge_hyper = False
+    e_accel_mult_boost = False
+
+    if chronos_accel_ratio != 32:
+        messages.append(f"Chronos to Accelerator ratio is {chronos_accel_ratio:d} instead of 32")
+        e_chronos_accel = True
+    if chronos_challenge_ratio != 4:
+        messages.append(f"Chronos to Challenge ratio is {chronos_challenge_ratio:d} instead of 4")
+        e_chronos_challenge_hyper = True
+    if hyper_challenge_ratio != 2:
+        messages.append(f"Hyper to Challenge ratio is {hyper_challenge_ratio:d} instead of 2")
+        e_chronos_challenge_hyper = True
     if accel_mult_ratio != 4:
-        print(f"Accel to Multiplier ratio is {accel_mult_ratio} instead of 4")
+        messages.append(f"Accel to Multiplier ratio is {accel_mult_ratio} instead of 4")
+        e_accel_mult_boost = True
     if boost_mult_ratio != 2:
-        print(f"Boost to Multiplier ratio is {boost_mult_ratio} instead of 2")
+        messages.append(f"Boost to Multiplier ratio is {boost_mult_ratio} instead of 2")
+        e_accel_mult_boost = True
 
-    console.print(ratios)
+    ratios.add_row("Chronos/Accel:", f"{'[bold red]' if e_chronos_accel else ''}{int(chronos_accel_ratio)}/1")
+    ratios.add_row(
+        "Chronos/Hyper/Challenge:",
+        f"{'[bold red]' if e_chronos_challenge_hyper else ''}{chronos_challenge_ratio:d}/{hyper_challenge_ratio:d}/1",
+    )
+    ratios.add_row(
+        "Accel/Boost/Mult:", f"{'[bold red]' if e_accel_mult_boost else ''}{accel_mult_ratio:d}/{boost_mult_ratio:d}/1"
+    )
+
+    return ratios, messages
 
 
-def calculated_stats(game: SynergismGame):
+def calculated_stats(game: SynergismGame) -> Table:
     stats = Table(title="Calculated Stats", box=box.MINIMAL_DOUBLE_HEAD)
     stats.add_column("Stat")
     stats.add_column("Value")
@@ -89,8 +121,31 @@ def calculated_stats(game: SynergismGame):
     stats.add_row("Current multiplier:", f"{game.multiplier:.3f}")
     stats.add_row("RT ascension count:", f"{game.current_ascension_timer:.3f}")
     stats.add_row("Total quarks:", f"{game.total_quarks:d}")
+    stats.add_row("Small hept inc:", f"{game.hepts_small_inc:.2f}")
+    stats.add_row("Hept per day:", f"{game.hept_per_day:.2e}", end_section=True)
+    stats.add_row("Orb to powder:", f"{game.orb_to_powder:.2f}")
+    stats.add_row("%Cube from powder:", f"{game.cube_from_powder*100:.2f}%")
+    stats.add_row("Powder goal:", f"{game.powder_goal:.2e}")
+    stats.add_row("Orbs to powder goal:", f"{game.orbs_to_powder_goal:.2e}")
 
-    console.print(stats)
+    return stats
+
+
+def print_buys(buys: ShopBuys, game: SynergismGame) -> Table:
+    buys_table = Table(box=box.MINIMAL_DOUBLE_HEAD, show_header=False)
+    buys_table.add_column("", style="bold")
+    buys_table.add_column("Item 1")
+    buys_table.add_column("Item 2")
+    buys_table.add_column("Benefit")
+    
+    buys_table.add_row("Accelrators", "Accel 1", "Accel 2", style="bold")
+    buys_table.add_row("", f"{buys.accel1:d}", f"{buys.accel2:d}", f"{game.shop_benefit_accel}", end_section=True)
+
+    buys_table.add_row("Wow Passes", "WoW 3", "WoW VY", style="bold")
+    buys_table.add_row("", f"{buys.wow3:d}", f"{buys.wowY:d}", f"{game.shop_benefit_hept}")
+
+    # console.print(Columns([accel_table, wow_table], equal=True))
+    return buys_table
 
 
 @logger.catch
@@ -114,15 +169,32 @@ def main() -> None:
 
     game.set_config(conf)
 
-    print_stats(game)
-
     sd = pandas.read_excel(f"{DATA_PATH}/Data.xlsx", sheet_name="Shop", header=[0, 1], engine="openpyxl")
-
     buys = ShopBuys(sd, game.total_quarks)
+    game.set_shop_benefits(buys)
 
-    # calculated_stats(game)
-    # console.rule()
-    # check_ratios(game)
+    balances = print_balances(game)
+
+    stats = calculated_stats(game)
+
+    console.print(Columns(balances, equal=True))
+    console.rule()
+    console.print(stats)
+
+    console.rule()
+    ratios, messages = check_ratios(game)
+    buys_table = print_buys(buys, game)
+
+    console.print(Columns([ratios, buys_table], equal=True))
+
+    if game.orbs_to_powder_goal <= 0:
+        messages.append("Level Chronos!")
+    else:
+        messages.append("Dunno yet!")
+
+    messages_print = Text("\n".join(messages))
+
+    console.print(Panel(messages_print, title="Messages", box=box.DOUBLE, expand=False))
 
 
 if __name__ == "__main__":
